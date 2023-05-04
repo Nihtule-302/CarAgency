9f3b61d05f2b182e8a99dbdc2689146c63212214
SaifMostafa0 <saifallah3@gmail.com>
2023-05-04 22:44:00 +0300

    Update Customer.java

--- Saif/Customer.java
+++ Saif/Customer.java
@@ -1,4 +1,35 @@
 package Saif;
 
-public class Customer {
+public class Customer extends Identification {
+    private Admin manager;
+    private static int idCounter = 0;
+
+    // No-arg constructor
+    public Customer() {
+        super();
+        idCounter++;
+        setId(idCounter);
+    }
+
+    // Constructor with name parameter
+    public Customer(String name) {
+        super(name);
+        idCounter++;
+        setId(idCounter);
+    }
+
+    // Getter for manager
+    public Admin getManager() {
+        return manager;
+    }
+
+    // Setter for manager
+    public void setManager(Admin manager) {
+        this.manager = manager;
+    }
+
+    // Getter for idCounter
+    public static int getIdCounter() {
+        return idCounter;
+    }
 }
