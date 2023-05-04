23173332d8435e003add8b244d44115cdc78fcb7
SaifMostafa0 <saifallah3@gmail.com>
2023-05-04 20:48:03 +0300

    Update Identification.java

--- Saif/Identification.java
+++ Saif/Identification.java
@@ -1,4 +1,34 @@
 package Saif;
 
 public class Identification {
+    private String name;
+    private int id;
+
+    // Void constructor
+    public Identification() {}
+
+    // Constructor with name parameter
+    public Identification(String name) {
+        this.name = name;
+    }
+
+    // Getter for id
+    public int getId() {
+        return id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for id
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
