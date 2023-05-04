27b70454eaecb2f70060ade6e5da27b397bce72b
peterelia22 <peterelia914@gmail.com>
2023-05-04 10:05:54 +0300

    ..

--- Peter/Admin.java
+++ Peter/Admin.java
@@ -1,4 +1,12 @@
 package Peter;
 
+import Ali.Employee;
+import Saif.Customer;
+
 public class Admin {
+    private Employee  employees = new Employee();
+    private Customer customers = new Customer();
+    private Car cars = new Car();
+    private TransactionManagement transactionManagement;
+
 }
