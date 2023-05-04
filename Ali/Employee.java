2a7f9af039522d325a3704ae680a251c9baf0904
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2023-05-04 12:24:29 +0300

    Update Employee.java
    1) Removed the employees array.
    
    2) Added an Object of type Admin called 'manager' to the class.
    
    3) When the non-empty constructor is called, the manager object is used to update the employees array.
    
    4) This means that the employees array is managed by the Admin class now

--- Ali/Employee.java
+++ Ali/Employee.java
@@ -1,43 +1,20 @@
 package Ali;
 
+import Peter.Admin;
 import Saif.Identification;
 
 public class Employee extends Identification {
+    private Admin manager = new Admin();
     private static int idCounter = 0;
-    private static Employee[] employees = new Employee[5];
     private double payCheck = 1000;
 
     public Employee(){
     }
     public Employee(String name){
         idCounter++;
+        setName(name);
         setId(idCounter);
-
-        int i = 0;
-        boolean flag = true;
-        while (i<employees.length && flag){
-            if(employees[i] == null){
-                employees[i] = this;
-                flag = false;
-            }
-            i++;
-        }
-
-        if(i == employees.length -1){
-            resizeArray();
-            employees[i+1] = this;
-        }
-    }
-
-    private void resizeArray(){
-        final int increaseBy = 1;
-        Employee[] temp = new Employee[employees.length + increaseBy];
-        System.arraycopy(employees, 0, temp, 0, employees.length);
-        employees = temp;
-    }
-
-    public static Employee[] getEmployees() {
-        return employees;
+        manager.addEmployee();
     }
 
     public void increasePayCheck(double money){
