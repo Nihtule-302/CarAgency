8993649776634b88aab4b43dae7a39d5d73c5546
peterelia22 <132546143+peterelia22@users.noreply.github.com>
2023-05-04 10:17:49 +0300

    Update Admin.java

--- Peter/Admin.java
+++ Peter/Admin.java
@@ -9,6 +9,6 @@ public class Admin {
     private Employee  employees = new Employee();
     private Customer customers = new Customer();
     private Car cars = new Car();
-    private TransactionManagement transactionManagement;
+    private TransactionManagement transactionManagement = new TransactionManagement();
 
 }
