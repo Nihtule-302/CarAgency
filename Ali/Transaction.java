3fe9718a61b46d46ad31bdaca5a63c34063fddac
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2023-05-05 19:20:30 +0300

    Update Transaction.java
    Now, when a customer buys or rents a car
    said car is removed from the cars array
    
    added findAvailableIndex() to optimize the code

--- Ali/Transaction.java
+++ Ali/Transaction.java
@@ -1,5 +1,6 @@
 package Ali;
 
+import Peter.Admin;
 import Saif.Car;
 import Saif.Customer;
 
@@ -8,6 +9,7 @@ public class Transaction extends TransactionManagement {
     private Customer customer;
     private Car car;
     private String paymentType;
+    private Admin manager = new Admin();
 
     public void setEmployeeAndCustomer(Employee employee, Customer customer) {
         this.employee = employee;
@@ -18,35 +20,37 @@ public class Transaction extends TransactionManagement {
         this.paymentType = cashOrInstallments;
         this.car = car;
         employee.increasePayCheck(1000);
+        manager.removeCar(car.getName());
     }
 
     public void rent(Car car){
         this.paymentType = "Rent";
         this.car = car;
         employee.increasePayCheck(100);
+        manager.removeCar(car.getName());
     }
 
     public void saveTransaction(){
         Transaction[] transactions = getTransactions();
-        int i = 0;
-        boolean flag = true;
-        while (i<transactions.length && flag){
-            if(transactions[i] == null){
-                transactions[i] = this;
-                flag = false;
-            }
-            i++;
-        }
+        int availableIndex = findAvailableIndex();
+        transactions[availableIndex] = this;
+    }
 
-        if(i == transactions.length -1){
-            resizeArray();
-            transactions[i+1] = this;
+    private int findAvailableIndex(){
+        Transaction[] transactions = getTransactions();
+        for (int i = 0; i < transactions.length; i++) {
+            if(transactions[i] == null)
+                return i;
         }
+        //if the array is full, resize array
+        //and return the last index
+        resizeArray();
+        return transactions.length -1;
+
     }
 
     private void resizeArray(){
-        final int increaseBy = 1;
-        Transaction[] temp = new Transaction[getTransactions().length + increaseBy];
+        Transaction[] temp = new Transaction[getTransactions().length + 1];
         System.arraycopy(getTransactions(), 0, temp, 0, getTransactions().length);
         setTransactions(temp);
     }
