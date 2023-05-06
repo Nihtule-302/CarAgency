aab70fec3cf289312179a5d922b213adf70e1344
peterelia22 <peterelia914@gmail.com>
2023-05-06 19:54:30 +0300

    Update Admin.java

--- Peter/Admin.java
+++ Peter/Admin.java
@@ -12,6 +12,7 @@ public class Admin {
     private Customer[] customers = new Customer[3];
     private Car[] cars = defaultCars();
     private TransactionManagement transactionManagement = new TransactionManagement();
+
     private Employee[] defaultEmployees() {
         Employee[] employees = new Employee[3];
         employees[0] = new Employee("Thorfinn");
@@ -19,12 +20,14 @@ public class Admin {
         employees[2] = new Employee("Fushi");
         return employees;
     }
-        private Car[] defaultCars() {
-            Car[] cars = new Car[2];
-            cars[0] = new Car("BMW",1232000,1755 );
-            cars[1] = new Car("MERCEDES",1848000,2156);
-            return cars;
-        }
+
+    private Car[] defaultCars() {
+        Car[] cars = new Car[2];
+        cars[0] = new Car("BMW", 1232000, 1755);
+        cars[1] = new Car("MERCEDES", 1848000, 2156);
+        return cars;
+    }
+
     private void resizeEmployee() {
         int increaseBy = 5;
         Employee[] temp = new Employee[employees.length + increaseBy];
@@ -47,8 +50,24 @@ public class Admin {
         cars = temp;
     }
 
+    private int findAvailableIndex(Object[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                return i;
+            }
 
-
-
+            if (arr instanceof Employee[]) {
+                resizeEmployee();
+                return arr.length - 1;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
 
