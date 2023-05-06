b394acbdbcc46b6d960581093ef2dbfa7e991d41
peterelia22 <peterelia914@gmail.com>
2023-05-06 12:34:55 +0300

    Update Admin.java

--- Peter/Admin.java
+++ Peter/Admin.java
@@ -6,12 +6,25 @@ import Saif.Car;
 import Saif.Customer;
 
 public class Admin {
-    private Employee  employees = new Employee();
-    private Customer customers = new Customer();
-    private Car cars = new Car();
-    private TransactionManagement transactionManagement = new TransactionManagement();
-    public Admin (){
-        
-    }
+    private Employee[] employees = defaultEmployees();
+    private Customer[] customers = new Customer[3];
+    private Car[] cars = defaultCars();
 
+
+    private TransactionManagement transactionManagement = new TransactionManagement();
+    private Employee[] defaultEmployees() {
+        Employee[] employees = new Employee[3];
+        employees[0] = new Employee("Thorfinn");
+        employees[1] = new Employee("Musashi");
+        employees[2] = new Employee("Fushi");
+        return employees;
+    }
+        private Car[] defaultCars() {
+            Car[] cars = new Car[2];
+            cars[0] = new Car("BMW");
+            cars[1] = new Car("MERCEDES");
+           
+            return cars;
+        }
+    }
 }
