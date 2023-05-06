dbe6b01cae837d124c3a25f2cf46b319298d25e6
peterelia22 <peterelia914@gmail.com>
2023-05-06 13:54:31 +0300

    Update Admin.java

--- Peter/Admin.java
+++ Peter/Admin.java
@@ -5,6 +5,8 @@ import Ali.TransactionManagement;
 import Saif.Car;
 import Saif.Customer;
 
+import java.util.Arrays;
+
 public class Admin {
     private Employee[] employees = defaultEmployees();
     private Customer[] customers = new Customer[3];
@@ -23,5 +25,22 @@ public class Admin {
             cars[1] = new Car("MERCEDES",1848000,2156);
             return cars;
         }
+    private void resizeEmployeeArray(int newSize) {
+        Employee[] temp = new Employee[newSize];
+        System.arraycopy(employees, 0, temp, 0, newSize);
+        employees = temp;
+    }
+
+    private void resizeCustomerArray(int newSize) {
+        Customer[] temp = new Customer[newSize];
+        System.arraycopy(customers, 0, temp, 0, newSize);
+        customers = temp;
+    }
+
+    private void resizeCarArray(int newSize) {
+        Car[] temp = new Car[newSize];
+        System.arraycopy(cars, 0, temp, 0, newSize);
+        cars = temp;
+    }
     }
 
