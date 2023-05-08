c35ffb5e498f909306ab7c5f89b6f3b63d9a20de
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2023-05-08 14:31:40 +0300

    Update AdminInterface.java

--- Ali/AdminInterface.java
+++ Ali/AdminInterface.java
@@ -8,22 +8,36 @@ public class AdminInterface {
     private Admin manager = new Admin();
 
     Scanner input = new Scanner(System.in);
-    public void chooseEmployeeCustomerCarOrTransactionManagement(){
+
+    public void start(){
+        System.out.println("Hello Admin");
+        chooseEmployeeCustomerCarOrTransactionManagement();
+    }
+
+    private void chooseEmployeeCustomerCarOrTransactionManagement(){
         System.out.println("| Employee | Customer | Car | TransactionManagement |");
         System.out.print("Choose from the list above the part of the system to access: ");
-
+        String choice = input.next();
+        switch (choice) {
+            case "Employee": employeeOptions();
+            break;
+            case "Customer": customerOptions();
+            break;
+            case "Car": carOptions();
+            break;
+        }
     }
-    public void employeeOptions(){
+    private void employeeOptions(){
         System.out.println("| Hire | Fire | Search | Display |");
         System.out.print("Choose from the list above the part of the system to access: ");
 
     }
-    public void customerOptions(){
+    private void customerOptions(){
         System.out.println("| Add | Remove | Search | Display |");
         System.out.print("Choose from the list above the part of the system to access: ");
 
     }
-    public void carOptions(){
+    private void carOptions(){
         System.out.println("| add | remove | Search | Display |");
         System.out.print("Choose from the list above the part of the system to access: ");
 
